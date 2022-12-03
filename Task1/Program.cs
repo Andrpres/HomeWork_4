a064adb8469594005dd050b5fd01e05ce289fed5
Andrpre <andrpre96@gmail.com>
2022-12-03 12:11:46 +0300

    Finish first task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool inCorrect = false;
+
+    while(!inCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            inCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Введите именно число");
+        }
+    }
+
+    return result;
+}
+
+int number1 = GetNumber("Введите число 1");
+int number2 = GetNumber("Введите число 2");
+int numberResult = number1;
+
+for (int i = 1; i < number2; i++)
+{
+    numberResult = numberResult * number1; 
+}
+
+Console.WriteLine($"{numberResult}");
